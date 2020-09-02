f7c722b4d4d065b2e61a43c5fef5ff30fac053ed
marcusbonifacio <marcusvbl@hotmail.com>
2020-09-02 20:22:59 -0300

    Portuguese names added

--- names_grabber.py
+++ names_grabber.py
@@ -2,9 +2,13 @@
 Search and get names from differents languages
 '''
 import csv
+import requests
+from bs4 import BeautifulSoup
+
+# Spanish names
 
 
-class spanish:
+class Spanish:
 
     def spanish_m():
         rep = set()
@@ -31,5 +35,41 @@ class spanish:
                         rep.add(first_name)
 
 
+# Portuguese names
+class Portuguese:
+
+    def portuguese_m():
+        start_url = (
+            'https://www.behindthename.com/names/gender/masculine/usage/portuguese')
+        names_site = requests.get(start_url).text
+        soup_names = BeautifulSoup(names_site, 'lxml')
+        next_page_url = ''
+        with open(".//lists/portuguese_m.txt", 'w+', encoding='utf-8') as f:
+            while next_page_url != start_url:
+                for name in soup_names.find_all('span', {'class': 'listname'}):
+                    f.write(f'{name.text.split(" ")[0]}\n')
+                next_parcial = soup_names.find_all(
+                    'nav', {'class': 'pagination'})[-1].find("a")['href']
+                next_page_url = f'https://www.behindthename.com{next_parcial}'
+                names_site = requests.get(next_page_url).text
+                soup_names = BeautifulSoup(names_site, "lxml")
+
+    def portuguese_f():
+        start_url = (
+            'https://www.behindthename.com/names/gender/feminine/usage/portuguese')
+        names_site = requests.get(start_url).text
+        soup_names = BeautifulSoup(names_site, 'lxml')
+        next_page_url = ''
+        with open(".//lists/portuguese_f.txt", 'w+', encoding='utf-8') as f:
+            while next_page_url != start_url:
+                for name in soup_names.find_all('span', {'class': 'listname'}):
+                    f.write(f'{name.text.split(" ")[0]}\n')
+                next_parcial = soup_names.find_all(
+                    'nav', {'class': 'pagination'})[-1].find("a")['href']
+                next_page_url = f'https://www.behindthename.com{next_parcial}'
+                names_site = requests.get(next_page_url).text
+                soup_names = BeautifulSoup(names_site, "lxml")
+
+
 if __name__ == '__main__':
-    spanish.spanish_f()
+    Portuguese.portuguese_f()
--- teste.py
+++ teste.py
@@ -1,8 +1,23 @@
-import csv
+class Car:
+    def __init__(self, max_speed):
+        self.max_speed = max_speed
+        self.current_speed = 0
 
-with open(".//lists/source/spanish_male.csv") as f:
-    reader = csv.reader(f)
-    with open(".//lists/spanish_m.txt", 'w+') as t:
-        for coluna in reader:
-            t.write(f'{coluna[0].split(" ")[0]}\n')
-            #print(coluna[0].split(" ")[0])
+    def throtle(self, delta):
+        self.current_speed += delta
+        if self.current_speed > self.max_speed: self.current_speed = 180
+        return self.current_speed
+    
+    def brake(self, delta):
+        self.current_speed -= delta
+        if self.current_speed < 0: self.current_speed = 0
+        return self.current_speed
+
+if __name__ == '__main__':
+    c1 = Car(180)
+
+    for _ in range(25):
+        print(c1.throtle(8))
+
+    for _ in range(10):
+        print(c1.brake(20))
